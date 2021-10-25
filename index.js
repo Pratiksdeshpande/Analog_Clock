b91ae7f5cb91ca094e9174831f32bc160d5535bf
Pratik <pdeshpande666@gmail.com>
2021-10-26 00:30:20 +0530

    Add digital clock to webpage

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ setInterval(() => {
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
+    
+    document.getElementById('hr').innerHTML = "<h3>Current Time: </h3> "+ htime + " Hrs " + mtime+" Min "+ stime+" Sec";
+
 
     hrotation = (30*htime) + (mtime/2);
     mrotation = 6*mtime;
@@ -11,5 +14,13 @@ setInterval(() => {
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
-
 }, 1000);
+d = new Date();
+date = d.getDate();
+month = d.getMonth();
+year = d.getYear();
+
+
+        
+
+
